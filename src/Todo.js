2d75d1548483eb2297a9b0b715b308ae694e85b9
ashvn24 <ashwinashi6@gmail.com>
2023-12-01 21:08:41 +0530

    added edit

--- src/Todo.js
+++ src/Todo.js
@@ -6,31 +6,45 @@ import { HiOutlineTrash } from "react-icons/hi2";
 function Todo() {
   const [Todo, setTodo] = useState("");
   const [todolist, setTodolist] = useState([]);
+  const [editList, setEditList] = useState(0);
 
   const addtodo = (event) => {
     event.preventDefault();
-    if (Todo !== "") { 
+    if (Todo !== "") {
       setTodolist([...todolist, { list: Todo, id: Date.now(), status: false }]);
     }
     setTodo("");
-    console.log(todolist);
+    if (editList) {
+      const todoEdit = todolist.map((to) =>
+        to.id === editList
+          ? (to = { id: to.id, list: Todo })
+          : (to = { id: to.id, list: to.list })
+      );
+      setTodolist(todoEdit);
+      setEditList(0);
+      setTodo("");
+    }
   };
 
-  const onDelete=(id)=>{
-    setTodolist(todolist.filter((list)=>list.id !== id))
-  }
+  const onDelete = (id) => {
+    setTodolist(todolist.filter((list) => list.id !== id));
+  };
 
-  const onComplete=(id)=>{
-    let Complete = todolist.map((list)=>{
-        if(list.id === id){
-            return{...list, status:!list.status}
-        }
-        return list
-    })
-    setTodolist(Complete)
-  }
+  const onComplete = (id) => {
+    let Complete = todolist.map((list) => {
+      if (list.id === id) {
+        return { ...list, status: !list.status };
+      }
+      return list;
+    });
+    setTodolist(Complete);
+  };
 
-  
+  const onEdit = (id) => {
+    const editTodo = todolist.find((to) => to.id === id);
+    setTodo(editTodo.list);
+    setEditList(editTodo.id);
+  };
 
   return (
     <div>
@@ -48,16 +62,28 @@ function Todo() {
               placeholder="Enter your Text"
               aria-label="default input example"
             />
-            <button className="btn btn-success mx-3 ml-2">Add</button>
+            <button className="btn btn-success mx-3 ml-2">{editList?'UPDATE':'ADD'}</button>
           </form>
           <ul className="my-2">
             {todolist.map((to) => (
-              <li className="list" id={to.status ? 'item':''}>
+              <li className="list" id={to.status ? "item" : ""}>
                 <div className="list-i">{to.list}</div>
                 <span>
-                  <MdFileDownloadDone className="icon tick" title="Complete" onClick={()=>onComplete(to.id)}/>
-                  <CiEdit className="icon edit" title="Edit" onClick={()=>onEdit(to.id)}/>
-                  <HiOutlineTrash className="icon trash" title="Delete" onClick={()=>onDelete(to.id)}/>
+                  <MdFileDownloadDone
+                    className="icon tick"
+                    title="Complete"
+                    onClick={() => onComplete(to.id)}
+                  />
+                  <CiEdit
+                    className="icon edit"
+                    title="Edit"
+                    onClick={() => onEdit(to.id)}
+                  />
+                  <HiOutlineTrash
+                    className="icon trash"
+                    title="Delete"
+                    onClick={() => onDelete(to.id)}
+                  />
                 </span>
               </li>
             ))}
